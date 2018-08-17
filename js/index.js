35d9cbe2a63fa86312b497a850dc5bc1318e3cf2
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-17 18:24:27 +0530

    upvote still working

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var author_all = [];
+var permlink_all = [];
+var rand;
+var glabal_val = 50;
 $(document).ready(function() {
   $('select').formSelect();
   $('.modal').modal({
@@ -8,9 +12,19 @@ $(document).ready(function() {
 
   //console.log("Hello");
 
+  steem.api.getDiscussionsByCreated({
+    "limit": 100
+  }, function(err, result) {
+    for (var i = 0; i < result.length; i++) {
+      author_all[i] = result[i].author;
+      permlink_all[i] = result[i].permlink;
+    }
+  });
+
 
 
 });
+
 
 function check_sign_in() {
   if (localStorage.getItem("voter") === null || localStorage.getItem("pass") === null || localStorage.getItem("voter") === "" || localStorage.getItem("pass") === "") {
@@ -21,13 +35,12 @@ function check_sign_in() {
     steem.api.getAccountVotes((localStorage.getItem("voter")), function(err, result) {
       var p_no;
       $('ul').empty();
-      for (i = 0; i < result.length; i++) {
+      for (var i = 0; i < result.length; i++) {
         p_no = i + 1;
         $('ul').append("<li class='collection-item'><p>" + p_no + ".</p><a href='http://steemit.com/@" + result[i].authorperm + "'>" + result[i].authorperm + "</a><p><p>Percent:" + result[i].percent + "</p><p>Weight:" + result[i].weight + "</p></li>");
       }
-      if(result == "" || err != null)
-      {
-          $('.modal').modal('open');
+      if (result == "" || err != null) {
+        $('.modal').modal('open');
       }
     });
   }
@@ -40,14 +53,13 @@ function sign_in() {
   if (localStorage.getItem("voter") !== null || localStorage.getItem("voter") !== "") {
     steem.api.getAccountVotes((localStorage.getItem("voter")), function(err, result) {
       var p_no;
-        $('ul').empty();
+      $('ul').empty();
       for (i = 0; i < result.length; i++) {
         p_no = i + 1;
         $('ul').append("<li class='collection-item'><p>" + p_no + ".</p><a href='http://steemit.com/@" + result[i].authorperm + "'>" + result[i].authorperm + "</a><p><p>Percent:" + result[i].percent + "</p><p>Weight:" + result[i].weight + "</p></li>");
       }
-      if(result == "" || err != null)
-      {
-          $('.modal').modal('open');
+      if (result == "" || err != null) {
+        $('.modal').modal('open');
       }
     });
   }
@@ -57,4 +69,21 @@ function sign_in() {
 
 function reSignin() {
   $('.modal').modal('open');
+}
+
+
+function weight(val) {
+  global_val = val;
+
+}
+
+function upvote() {
+  rand = Math.floor(Math.random() * (99 - 1 + 1) + 1);
+  // Still have to implement the function that if already a vote done over index on array or permlink....
+
+  var activeWif = steem.auth.toWif(localStorage.getItem("voter"), localStorage.getItem("pass"), 'active');
+  steem.broadcast.vote(activeWif, localStorage.getItem("voter"), author_all[rand], permlink_all[rand], global_val, function(err, result) {
+    console.log(err, result);
+  });
+
 }
