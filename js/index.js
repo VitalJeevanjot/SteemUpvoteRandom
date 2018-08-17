e7523c63d51138315406be0eebd8ea27cefce169
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-17 16:49:33 +0530

    voting List Showing at start

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $('select').formSelect();
   //console.log("Hello");
-  localStorage.setItem("voter", "genievot");
-//If localstorage name and password is not null.{
-  steem.api.getAccountVotes((localStorage.getItem("voter")), function(err, result) {
-    console.log(err, result);
-  });
-  //}
+  localStorage.setItem("voter", "jeevanjot");
+
+  if (localStorage.getItem("voter") !== null) {
+    steem.api.getAccountVotes((localStorage.getItem("voter")), function(err, result) {
+      var p_no;
+      for (i = 0; i < result.length; i++) {
+        p_no = i+1;
+        $('ul').append("<li class='collection-item'><p>"+ p_no +".</p><a href='http://steemit.com/@" + result[i].authorperm + "'>" + result[i].authorperm + "</a><p><p>Percent:" + result[i].percent + "</p><p>Weight:" + result[i].weight + "</p></li>");
+      }
+    });
+  }
 });
